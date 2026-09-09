16281499aa6f5a2aa37c09072c017fe0fa3df8e8
Yeonkyung-Jeong <edaily0129@gmail.com>
2026-09-09 23:05:27 +0900

    문제 추가

---
+++ BFS/프로그래머스 단어변환.cpp
@@ -0,0 +1,57 @@
+// 14m
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int answer = 0;
+
+bool compare(string begin, string next) {
+    int cnt = 0;
+
+    for (int i = 0; i < begin.size(); i++) {
+        if (begin[i] != next[i]) cnt++;
+    }
+
+    return cnt == 1;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    auto it = find(words.begin(), words.end(), target);
+    if (it == words.end()) return 0;
+    if (begin.size() != target.size()) return 0;
+
+    queue<string> q;
+    q.push(begin);
+
+    unordered_map<string, int> dist;
+    for (string& w : words) dist[w] = -1;
+    dist[begin] = 0;
+
+    while (!q.empty()) {
+        string cur = q.front();
+        q.pop();
+
+        // 종료조건
+        if (cur == target) {
+            answer = dist[cur];
+            break;
+        }
+
+        // bfs 조건
+        for (int i = 0; i < words.size(); i++) {
+            if (dist[words[i]] != -1) continue;
+            if (!compare(cur, words[i])) continue;
+
+            string next = words[i];
+            dist[next] = dist[cur] + 1;
+            q.push(next);
+        }
+    }
+
+    return answer;
+
+}
