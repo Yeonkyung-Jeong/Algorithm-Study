dc76acc074615a4bb70b5b13c503066f69a766f8
Yeonkyung-Jeong <edaily0129@gmail.com>
2026-09-15 23:33:44 +0900

    문제추가

---
+++ BFS/프로그래머스 거리두기확인하기.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> dist;
+
+int dr[4] = { 0, 1, 0, -1 };
+int dc[4] = { 1, 0, -1, 0 };
+
+int bfs(vector<string>& room) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (room[i][j] != 'P') continue;
+
+            dist.assign(5, vector<int>(5, -1));
+            dist[i][j] = 0;
+
+            queue<pair<int, int>> q;
+            q.push({ i, j });
+
+            while (!q.empty()) {
+                int curr = q.front().first, curc = q.front().second;
+                q.pop();
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = curr + dr[d];
+                    int nc = curc + dc[d];
+
+                    if (nr < 0 || nc < 0 || nr >= 5 || nc >= 5) continue;
+                    if (room[nr][nc] == 'X') continue;
+                    if (dist[nr][nc] != -1) continue;
+
+                    dist[nr][nc] = dist[curr][curc] + 1;
+
+                    if (room[nr][nc] == 'P') {
+                        if (dist[nr][nc] == 2) {
+                            if (room[curr][curc] == 'O' || room[curr][curc] == 'P') return 0;
+                            continue;
+                        }
+
+
+
+                        if (dist[nr][nc] == 1) {
+                            if (room[curr][curc] == 'P') return 0;
+                        }
+                    }
+
+                    if (dist[nr][nc] < 2) q.push({ nr,nc });
+                }
+            }
+
+        }
+    }
+
+    return 1;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> answer;
+
+    // places 순회를 해서 bfs 호출 
+    for (vector<string>& v : places) answer.push_back(bfs(v));
+
+    return answer;
+}
